5631b2cfe20900b1204a34d671daaa208b931880
Diego Adan Perez Zetina <98625600+KaliZSH4DE@users.noreply.github.com>
2025-02-24 20:14:52 -0600

    Add files via upload

--- docs/validar.js
+++ docs/validar.js
@@ -1,21 +1,12 @@
-
-function sanitizarHtml() {
+function verificarHtml() {
     let input = document.getElementById("inputHtml").value;
     let parser = new DOMParser();
     let doc = parser.parseFromString(input, "text/html");
 
-    
-    doc.querySelectorAll("script, style").forEach(el => el.remove());
-
-    doc.querySelectorAll("iframe").forEach(el => el.remove());
-
-    doc.querySelectorAll("object, embed").forEach(el => el.remove());
-
-    doc.querySelectorAll("*").forEach(el => {
-        Array.from(el.attributes).forEach(attr => {
-            if (attr.name.startsWith("on")) el.removeAttribute(attr.name);
-        });
-    });
-    
-    document.getElementById("outputHtml").innerHTML = doc.body.innerHTML;
+    // Verifica si hay alguna etiqueta en el contenido
+    if (doc.body.children.length > 0) {
+        alert("NEEEEEEEEEEL PERRO tu no pasas");
+    } else {
+        alert("Texto fino de traje elegante apto para pasar");
+    }
 }
